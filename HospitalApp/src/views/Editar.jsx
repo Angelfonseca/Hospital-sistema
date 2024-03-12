dc21955092952198992b58aea2c0e0f3858769e9
Alejandro Guevara de Luna <alejandrogdlcecyt@gmail.com>
2024-03-11 23:15:29 -0600

    AddWIP

--- HospitalApp/src/views/Editar.jsx
+++ HospitalApp/src/views/Editar.jsx
@@ -1,7 +1,101 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
+
+//Images
+import EditIcon from '/src/assets/Edit_Icon.png';
+
+//Styles
+import '/src/styles/Editar.css';
 
 export default function Editar() {
   return (
-    <div>Editar</div>
+    <div className='ContainerFull_Edit'>
+      <div>
+        <Link to="/"><button id='BTN_EditRegresar'>Regresar</button></Link>
+      </div>
+      <form className='Con_Form'>
+        <div className='Con_Search'>
+          <div>
+            <img src={EditIcon} alt="Edit Icon" />
+            <h2 id='NoMargin'>Editar Producto</h2>
+          </div>
+          <div>
+            <p id='NoMargin'>Código:</p>
+            <input type="search" name="" id="Input_EditSearch_Pro" placeholder='Escane el código del producto que deseas editar'/>
+          </div>
+        </div>
+
+        <div className='Con_Inputs'>
+
+          <div>
+            <div>
+              <p id='Edit_Font18'>Asignado:</p>
+              <input type="text" id='Inputs_Edit_Pro' placeholder='Asignado de HPGA'/>
+            </div>
+            <div>
+              <p id='Edit_Font18'>Clave Cabms:</p>
+              <input type="text" id='Inputs_Edit_Pro' placeholder='Clave Cabms (Nombre)'/>
+            </div>
+            <div>
+              <p id='Edit_Font18'>Cosecutivo:</p>
+              <input type="text" id='Inputs_Edit_Pro' placeholder='Consecutivo (Apellido)'/>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <p id='Edit_Font18'>Descripción:</p>
+              <input type="text" name="" id="Input_EditDesc_Pro" placeholder='Descripción detallada del producto'/>
+            </div>
+            <div>
+              <p id='Edit_Font18'>Costo:</p>
+              <input type="text" id='Inputs_Edit_Pro' placeholder='Valor del bien' />
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <p id='Edit_Font18'>Marca:</p>
+              <input type="text" id='Inputs_Edit_Pro' placeholder='Marca'/>
+            </div>
+            <div>
+              <p id='Edit_Font18'>Modelo:</p>
+              <input type="text" id='Inputs_Edit_Pro' placeholder='Modelo'/>
+            </div>
+            <div>
+              <p id='Edit_Font18'>Serie:</p>
+              <input type="text" id='Inputs_Edit_Pro' placeholder='Serie'/>
+            </div>
+          </div>
+
+
+          <div>
+            <div>
+              <p id='Edit_Font18'>Motor:</p>
+              <input type="text" id='Inputs_Edit_Pro' placeholder='Motor'/>
+            </div>
+            <div>
+              <p id='Edit_Font18'>Recurso:</p>
+              <input type="text" id='Inputs_Edit_Pro' placeholder='Recurso'/>
+            </div>
+            <div>
+              <p id='Edit_Font18'>Responsable:</p>
+              <input type="text" id='Inputs_Edit_Pro' placeholder='Responsable'/>
+            </div>
+          </div>
+
+          <div>
+            <p id='Edit_Font18'>Área o Ubicación</p>
+            <input type="text" name="" id="Inputs_Edit_Pro"  placeholder='Área-Ubicación del bien'/>
+          </div>
+          
+        </div>
+
+        <div className='Con_BTNEdit'>
+            <button type='submit' id='BTNDelete_Pro'>Eliminar</button>
+            <button type='submit' id='BTNEdit_Pro'>Guardar</button>
+        </div>
+
+      </form>
+    </div>
   )
 }
--- HospitalApp/src/views/Reportes.jsx
+++ HospitalApp/src/views/Reportes.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
+
+//Styles
+import '/src/styles/Reportes.css';
+
 
 export default function Reportes() {
   return (
-    <div>Reportes</div>
+    <div className='Container_Reports'>
+      <div>
+      <Link to="/"><button id='btnRegresarRep'>Regresar</button></Link>
+      </div>
+      <div>
+        <input type="text" id='SearchID'/>
+        <select name="" id="SelectGroup"></select>
+      </div>
+      <div>
+        <table id='TableProducts'>
+          <thead>
+            <th>Idetificador</th>
+            <th>Descripción</th>
+            <th>Costo</th>
+            <th>Marca</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1234567890</td>
+              <td>Laptop HP 15pulgadas</td>
+              <td>$15000</td>
+              <td>HP</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <button id='btn_Excel'>Exportar a Excel</button>
+      </div>
+    </div>
   )
 }
