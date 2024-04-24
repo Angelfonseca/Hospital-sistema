f6239ac85d67ae5da1ac1caf0d36957a4657217c
Angel Isaac Fonseca Gómez <Magnua321@gmail.com>
2024-04-24 16:08:15 -0600

    abrir a red local
    Se abre el front a red local

--- HospitalApp/vite.config.js
+++ HospitalApp/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Escucha en todas las interfaces de red
+    port: 5173,
+  },
+});
